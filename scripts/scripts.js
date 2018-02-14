e4d658007f4013b80278b4ac6847c06cbe83d06d
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-02-14 14:07:27 -0800

    This adds the input forms on the top and their functionality. Other than that it should all be the same as the last commit, minus a few deletions for tidiness.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,23 @@
 'use strict'
+//Just variable declarations.
 var hours=[7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 var locations=['Pike Place','Seattle Center','Red Square','Edmonds Beach','King Street Station']
-var access=['pikePlace','seattleCenter','redSquare','edmondsBeach','kingStreetStation']
-var accessTable=['pikePlaceTable','seattleCenterTable','redSquareTable','edmondsBeachTable','kingStreetStationTable']
+var access=['Pike Place','Seattle Center','Red Square','Edmonds Beach','King Street Station']
+var accessTable=['Pike Place Table','Seattle Center Table','Red Square Table','Edmonds Beach Table','King Street Station Table']
 var minCustomersPerHour=[23,20,15,5,30]
 var maxCustomersPerHour=[65,90,60,75,60]
 var avgCookiesPerCustomer=[6.3,12.2,5.1,15.5,3.9]
 var prices=[.5,1,.5,.75,.5]
 var stands=[]
+//Establishment of the lower table in the DOM.
 var profitTable=document.getElementById('sales')
-for(var i=0;i<locations.length;i++)
-{
-    stands.push(new Stand(i));
-    stands[i].calcCustomersEachHour();
-    stands[i].calcCookiesEachHour();
-    stands[i].calcTotalCookiesPerDay();
-}
+//This is the creation of the objects for the website's forms.
+var newLocation=document.getElementById('newLocation')
+var newMinCust=document.getElementById('newMinCust')
+var newMaxCust=document.getElementById('newMaxCust')
+var newAvgCook=document.getElementById('newAvgCook')
+var submitButtonPress=document.getElementById('submitButton')
+//This is the constructor function for each Stand.
 function Stand(i) {
     this.locationName=locations[i];
     this.minCustomersPerHour=minCustomersPerHour[i];
@@ -61,52 +63,28 @@ function Stand(i) {
         this.profitByHour.push(this.cookiesSoldEachHour[h]*this.price);
         }
     }
-    this.toDOMList=function()
-    {
-    for(var h=0;h<hours.length;h++)
-        {
-        var liEl=document.createElement('li');
-        liEl.textContent=hours[h]+' : '+this.cookiesSoldEachHour[h]+' cookies, '+this.profitByHour[h]+'$';
-        var parentUl=document.getElementById(this.access);
-        parentUl.appendChild(liEl);
-        }
-    liEl=document.createElement('li');
-    liEl.textContent='Total: '+this.totalCookiesSoldPerDay+' cookies';
-    parentUl.appendChild(liEl);
-    liEl=document.createElement('li');
-    liEl.textContent='Total Profit: '+this.totalProfit+'$';
-    parentUl.appendChild(liEl);
-    }
-    this.toDOMTable=function()
-    {
-        getElementbyId(this.accessTable)
-        var trEl=document.createElement('tr');
-        var tdEl=document.createElement('td');
-        tdEl.textContent=this.locationName
-    }
-    this.toDOMTableHeader=function()
-    {
-        var trEl=document.createElement('tr');
-        var thEl=document.createElement('th');
-        thEl.textContent=this.locationName;
-        trEl
-        for(h=0;h<hours.length;h++)
-        {
-
-        }
-        trEl.appendChild(thEl);
-    }
     this.calcCustomersEachHour()
     this.calcCookiesEachHour()
     this.calcProfitByHour()
     this.calcTotalCookiesPerDay()
     this.calcTotalProfit()
 }
+for(var i=0;i<locations.length;i++)
+{
+    stands.push(new Stand(i));
+    stands[i].calcCustomersEachHour();
+    stands[i].calcCookiesEachHour();
+    stands[i].calcTotalCookiesPerDay();
+}
+//This randomizer determines how many customers arrive.
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+//This renders the table.
 function toDOMTable()
 {
+//This makes the objects
+//This renders the objects.
     var trEl=document.createElement('tr');
     var thEl=document.createElement('th');
     thEl.textContent='Locations';
@@ -160,5 +138,33 @@ function toDOMTable()
     trEl.appendChild(tdEl);
     profitTable.appendChild(trEl);
     console.log(total);
+}
+submitButtonPress.addEventListener('click',function()
+{
+    if(newMinCust.value===''||newMaxCust.value===''||newLocation.value===''||newAvgCook.value===''){
+    alert('Please input a value into each field.')
+}    else{
+    addStore();
+}
+}, false);
+function addStore()
+{
+    locations.push(newLocation.value)
+    access.push(newLocation.value)
+    accessTable.push(newLocation.value+' Table')
+    minCustomersPerHour.push(Number(newMinCust.value))
+    maxCustomersPerHour.push(Number(newMaxCust.value))
+    avgCookiesPerCustomer.push(Number(newAvgCook.value))
+    newLocation.value=""
+    newAvgCook.value=""
+    newMaxCust.value=""
+    newMinCust.value=""
+    profitTable.innerHTML=""
+    i=minCustomersPerHour.length-1
+    stands.push(new Stand(i));
+    stands[i].calcCustomersEachHour();
+    stands[i].calcCookiesEachHour();
+    stands[i].calcTotalCookiesPerDay();
+    toDOMTable();
 }
 toDOMTable();
